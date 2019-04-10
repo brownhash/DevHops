3378dfc5bd54a7ca1b76ddef954e1366649f0cfa
Harshit Sharma <sharma1612harshit@gmail.com>
2019-04-10 22:34:13 +0530

    DevHops File

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/result',methods = ['POST'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+f=open("count.txt","w")
+f.write("0")
+f.close()
 
-@app.route('/terminal')
-def terminal():
-    return render_template("terminal.html",result = result)
+@app.route("/loadbalancer")
+def loadbalancer():
+    f=open("count.txt","r")
+    r=f.read()
+    counter=int(r)
+    f.close()
 
-@app.route('/term',methods = ['POST'])
-def term():
-   if request.method == 'POST':
-       sudo_com="sudo -p "
+    r_string="Simultaneous logins : "+str(counter)
+    return(r_string)
+
+@app.route("/login",methods = ['POST'])
+def login():
+    if request.method == 'POST':
        result = request.form
-       sudo=result.get("sudo")
-       password=str(result.get("password"))
-       command=str(result.get("command"))
-       if(sudo == "sudoyes"):
-           sudo_com+=password
-           sudo_command=sudo_com+" "+command
-           command=sudo_command
-       import os
-       term_result=os.popen(command).readlines()
-       s=""
-       for i in term_result:
-           s+=str(i)+" | "
-       return (s)
+       user=result.get("username")
+       passw=result.get("password")
+       if(user=="admin" and passw=="root"):
+           f=open("count.txt","r")
+           r=f.read()
+           counter=int(r)
+           f.close()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+           counter+=1
+
+           f=open("count.txt","w")
+           f.write(str(counter))
+           f.close()
+           return render_template("main.html",result = user)
+       else:
+           return render_template("loginerror.html")
+
+@app.route("/logout")
+def logout():
+    f=open("count.txt","r")
+    r=f.read()
+    counter=int(r)
+    f.close()
+
+    counter-=1
+
+    f=open("count.txt","w")
+    f.write(str(counter))
+    f.close()
+    return render_template("index.html")
