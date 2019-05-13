446637ee5c2a7ca8dd075ffcd303257e92a8098b
Harshit Sharma <sharma1612harshit@gmail.com>
2019-05-13 20:31:14 +0530

    DevHops File

--- main.py
+++ main.py
@@ -67,5 +67,43 @@ def load():
     f.close()
     return (counter)
 
+@app.route('/password_gen')
+def password_gen():
+    import random as rand
+    l = ["#", "*"]
+    l1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+          'w', 'x', 'y', 'z']
+    l2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+          'W', 'X', 'Y', 'Z']
+    l3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+    a = rand.randint(2, 3)
+    b = rand.randint(2, 3)
+    c = rand.randint(1, 2)
+    rand.shuffle(l1)
+    rand.shuffle(l2)
+    rand.shuffle(l3)
+    rand.shuffle(l)
+    s1 = ""
+    s = ""
+    for i in range(0, a):
+        s += str(l2[i])
+    for i in range(0, a):
+        s += str(l3[i])
+    for i in range(0, a):
+        s += str(l1[i])
+    for i in l:
+        s += str(i)
+    rand.shuffle(l2)
+    for i in range(0, c):
+        s1 += str(l2[i])
+
+    result = list(s)
+    rand.shuffle(result)
+    s2 = ""
+    for i in result:
+        s2 += i
+    return(s1 + s2)
+
 if __name__ == "__main__":
     app.run(debug=True)
